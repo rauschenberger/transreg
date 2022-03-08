15a9e5d6a93b31a6cf7ff6801a979de4d4655dcb
Armin <armin.rauschenberger@uni.lu>
2022-03-08 16:30:30 +0100

    test for coefs render preds

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -4,33 +4,69 @@
 n <- 100; p <- 1000
 X <- matrix(stats::rnorm(n*p),nrow=n,ncol=p)
 beta <- stats::rnorm(p)
-prior <- beta + stats::rnorm(p)
+prior1 <- beta + stats::rnorm(p,sd=0.1)
+prior2 <- beta + stats::rnorm(p,sd=0.1)
 y <- X %*% beta
 
-slow <- iso.slow.single(y=y,X=X,prior=matrix(prior,ncol=1),family="gaussian")$beta
-fast <- iso.fast.single(y=y,X=X,prior=matrix(prior,ncol=1),family="gaussian")$beta
-
-table(prior=sign(prior),sign(slow)) # okay
-table(prior=sign(prior),sign(fast)) # solve this problem
-table(old=sign(slow),new=sign(fast))
+slow <- iso.slow.single(y=y,X=X,prior=matrix(prior1,ncol=1),family="gaussian")$beta
+fast <- iso.fast.single(y=y,X=X,prior=matrix(prior1,ncol=1),family="gaussian")$beta
 
 testthat::test_that("expected signs (slow)",{
-  cond1 <- all(slow[prior==0]==0)
-  cond2 <- all(slow[prior<0]<=0)
-  cond3 <- all(slow[prior>0]>=0)
+  cond1 <- all(slow[prior1==0]==0)
+  cond2 <- all(slow[prior1<0]<=0)
+  cond3 <- all(slow[prior1>0]>=0)
   testthat::expect_true(cond1&cond2&cond3)
 })
 
 testthat::test_that("expected signs (fast)",{
-  cond1 <- all(fast[prior==0]==0)
-  cond2 <- all(fast[prior<0]<=0)
-  cond3 <- all(fast[prior>0]>=0)
+  cond1 <- all(fast[prior1==0]==0)
+  cond2 <- all(fast[prior1<0]<=0)
+  cond3 <- all(fast[prior1>0]>=0)
   testthat::expect_true(cond1&cond2&cond3)
 })
 
 testthat::test_that("correlation (slow, fast)",{
-  cor <- stats::cor(slow,fast)
-  testthat::expect_true(cor>0.99)
+  cond1 <- abs(mean(slow)-mean(fast))<0.01
+  cond2 <- stats::cor(slow,fast)>0.99
+  testthat::expect_true(cond1&cond2)
 })
+
+#--- equivalence predicted values ---
+
+for(scale in c("exp","iso")){
+  
+  family <- "gaussian"
+  
+  prior <- cbind(prior1,prior2)
+  object <- transreg(y=y,X=X,prior=prior,family=family,scale=scale)
+  y_hat1 <- predict(object,newx=X)
+  
+  beta <- coef(object$base,s=c(object$meta$lambda.min,object$meta$lambda.1se))
+  omega <- as.numeric(coef(object$meta,s=object$meta$lambda.min))
+  names <- paste0("source",seq_len(ncol(prior)))
+  names(omega) <- c("(Intercept)",names,"lambda.min","lambda.1se")
+  
+  alpha_star <- omega["(Intercept)"] + omega["lambda.min"]*beta["(Intercept)","s1"] + omega["lambda.1se"]*beta["(Intercept)","s2"]
+  beta_star <- rep(NA,times=p)
+  
+  if(scale=="exp"){
+    for(j in seq_len(p)){
+      beta_star[j] <- sum(omega[names]*object$base$prior$gamma*sign(object$base$z[j,])*abs(object$base$z[j,])^object$base$prior$tau) + omega["lambda.min"]*beta[1+j,"s1"] + omega["lambda.1se"]*beta[1+j,"s2"]
+    }
+  }
+  if(scale=="iso"){
+    for(j in seq_len(p)){
+      beta_star[j] <- sum(omega[names]*object$base$prior$beta[j,]) + omega["lambda.min"]*beta[1+j,"s1"] + omega["lambda.1se"]*beta[1+j,"s2"]
+    }
+  }
+  y_hat2 <- joinet:::.mean.function(alpha_star + X %*% beta_star,family=family)
+  
+  testthat::test_that("correlation (pred, coef)",{
+    cond1 <- mean(y_hat1)-mean(y_hat2) < 0.01
+    cond2 <- stats::cor(y_hat1,y_hat2) > 0.99
+    testthat::expect_true(cond1&cond2)
+  })
+  
+}
 
 
