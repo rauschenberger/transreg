10043679f2c2d545f542ffe9448bfddc16031218
Armin <armin.rauschenberger@uni.lu>
2022-03-08 09:17:32 +0100

    add pkgname

---
+++ R/pkgname.R
@@ -0,0 +1,30 @@
+#' @name transreg-package
+#' @keywords documentation
+#' @docType package
+#' 
+#' @title
+#' Penalised regression with multiple sets of prior coefficients
+#' 
+#' @description
+#' The R package \code{starnet} implements penalised regression
+#' with multiple sets of prior coefficients.
+#' 
+#' @details
+#' Use function \code{\link{transreg}} for model fitting.
+#' Type \code{library(transreg)} and then \code{?transreg} or
+#' \code{help("transreg)"} to open its help file.
+#' 
+#' See the vignette for further examples.
+#' Type \code{vignette("transreg")} or \code{browseVignettes("transreg")}
+#' to open the vignette.
+#' 
+#' @references
+#' A Rauschenberger, MA van de Wiel, and E Glaab (2022).
+#' "Penalised regression with multiple sets of prior coefficients".
+#' \emph{Manuscript in preparation.}
+#' \email{armin.rauschenberger@uni.lu}
+#'
+#' @examples
+#' NA
+#' 
+NULL
