e9430dd1a948161b654d8a910a26900da9134492
Armin <armin.rauschenberger@uni.lu>
2022-12-01 14:45:15 +0100

    test without CVXR

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -9,25 +9,26 @@ y <- X %*% beta
 #- - - slow and fast isotonic scaling- - - - - - - - - - - - - - - - - - - - - -
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+prior1 <- beta + stats::rnorm(p,sd=0.1)
+prior2 <- beta + stats::rnorm(p,sd=0.1)
+
+fast <- .iso.fast.single(y=y,X=X,prior=matrix(prior1,ncol=1),family="gaussian")$beta
+  
+testthat::test_that("expected signs (fast)",{
+  cond1 <- all(fast[prior1==0]==0)
+  cond2 <- all(fast[prior1<0]<=0)
+  cond3 <- all(fast[prior1>0]>=0)
+  testthat::expect_true(cond1&cond2&cond3)
+})
+
 if("CVXR" %in% rownames(installed.packages())){
   
-  prior1 <- beta + stats::rnorm(p,sd=0.1)
-  prior2 <- beta + stats::rnorm(p,sd=0.1)
-  
   slow <- .iso.slow.single(y=y,X=X,prior=matrix(prior1,ncol=1),family="gaussian")$beta
-  fast <- .iso.fast.single(y=y,X=X,prior=matrix(prior1,ncol=1),family="gaussian")$beta
-  
+
   testthat::test_that("expected signs (slow)",{
     cond1 <- all(slow[prior1==0]==0)
     cond2 <- all(slow[prior1<0]<=0)
     cond3 <- all(slow[prior1>0]>=0)
-    testthat::expect_true(cond1&cond2&cond3)
-  })
-  
-  testthat::test_that("expected signs (fast)",{
-    cond1 <- all(fast[prior1==0]==0)
-    cond2 <- all(fast[prior1<0]<=0)
-    cond3 <- all(fast[prior1>0]>=0)
     testthat::expect_true(cond1&cond2&cond3)
   })
   
@@ -90,3 +91,4 @@ for(scale in c("exp","iso")){
     
   }
 }
+
