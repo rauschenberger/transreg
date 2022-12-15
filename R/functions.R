d395ac4a1bcd57054f871396fe4cce0da7b90ae2
Armin <armin.rauschenberger@uni.lu>
2022-12-15 10:48:33 +0100

    replace Inf by large number

--- R/functions.R
+++ R/functions.R
@@ -632,7 +632,8 @@ NULL
         coefs[i,2] <- ifelse(is.na(temp["eta"]),0,temp["eta"])
         pred[,i] <- stats::fitted(glm)
       } else {
-        if(sd(eta)==0){eta <- stats::rnorm(n); lambda <- Inf}else{lambda <- 0}
+        #if(sd(eta)==0){stop("debug")}
+        if(sd(eta)==0){eta <- eta + stats::rnorm(n,sd=1e-06); lambda <- 99e99}else{lambda <- 0}
         glmnet <- glmnet::glmnet(x=cbind(0,eta),y=y,family=family,lambda=lambda,lower.limits=0,intercept=TRUE)
         #temp <- stats::coef(glmnet)
         temp <- glmnet::coef.glmnet(glmnet)
