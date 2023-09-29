0e22bd9f973854871fa12ccd96d8d0faf547c365
Armin <armin.rauschenberger@uni.lu>
2023-09-29 18:26:45 +0200

    rename orcid file

--- R/pkgname.R
+++ R/pkgname.R
@@ -21,13 +21,13 @@
 #' 
 #' @references
 #' Armin Rauschenberger 
-#' [![AR](orcid_16x16.png "(iD)")](https://orcid.org/0000-0001-6498-4801),
+#' [![AR](orcid.png "(iD)")](https://orcid.org/0000-0001-6498-4801),
 #' Zied Landoulsi
-#' [![ZL](orcid_16x16.png "(iD)")](https://orcid.org/0000-0002-2327-3904),
+#' [![ZL](orcid.png "(iD)")](https://orcid.org/0000-0002-2327-3904),
 #' Mark A. van de Wiel 
-#' [![MvdW](orcid_16x16.png "(iD)")](https://orcid.org/0000-0003-4780-8472),
+#' [![MvdW](orcid.png "(iD)")](https://orcid.org/0000-0003-4780-8472),
 #' and Enrico Glaab
-#' [![EG](orcid_16x16.png "(iD)")](https://orcid.org/0000-0003-3977-7469)
+#' [![EG](orcid.png "(iD)")](https://orcid.org/0000-0003-3977-7469)
 #' (2022).
 #' "Penalised regression with multiple sets of prior effects".
 #' *Manuscript in preparation.*
